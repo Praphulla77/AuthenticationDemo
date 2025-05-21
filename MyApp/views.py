c4be39f89c417bdc95462af67680461710959ef0
Praphull Subhash Sangle <sangalepraphulla9689@gmail.com>
2025-05-21 15:03:41 -0800

    first commit

---
+++ MyApp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
+
+
+# Create your views here.
+def welcome(request):
+    return render(request,"MyApp/welcome.html")
+
+def register(request):
+    if request.method == "GET":
+        form = UserCreationForm()
+        return render(request,'registration/register.html',{"form":form})
+    else:
+        form = UserCreationForm(request.POST)
+        if(form.is_valid()):
+            form.save()
+        else:
+            return HttpResponse("Error")
+        return redirect("/")
